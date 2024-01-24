dfa482cbdda0107a9628fd5bb487dc91a3f111db
vtb4097740 <kupreychik@vtb.ru>
2024-01-25 00:46:09 +0300

    add new tests

--- src/test/java/dev/com/kupreychik/maths/MathTest.java
+++ src/test/java/dev/com/kupreychik/maths/MathTest.java
@@ -3,11 +3,18 @@ package dev.com.kupreychik.maths;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class MathTest {
     private final Math math = new Math();
 
     @Test
     void throw_exception_when_first_agg_is_null() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> math.add(null, 1));
+    }
+
+    @Test
+    void should_return_true(){
+        assertTrue(true);
     }
 }
